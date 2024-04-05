10109f37540d3afb247b28857661375597c338d0
renad-lab <renad@pursuit.org>
2024-04-05 03:28:57 -0400

    Updated lab

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-document.getElementById("button").addEventListener("click", function () {
-  const container = document.getElementById("container");
-  const squares = container.querySelectorAll(".square");
-  const lastSquare = squares[squares.length - 1];
-  const newSquare = document.createElement("div");
-  newSquare.classList.add("square");
-  newSquare.style.width = parseInt(lastSquare.style.width) + 20 + "px"; //
-  newSquare.style.height = parseInt(lastSquare.style.height) + 20 + "px"; //
-  container.appendChild(newSquare);
+const body = document.querySelector("body");
+const button = document.createElement("button");
+
+button.textContent = "Enter";
+body.appendChild(button);
+const buttonSize = getElementSize(button);
+
+const border = document.createElement("div");
+border.id = "current";
+border.style.width = "90vw";
+border.style.height = "70vh";
+border.style.border = "10px solid black";
+body.appendChild(border);
+
+let canClear = false;
+
+button.addEventListener("click", (event) => {
+  const currentDiv = document.getElementById("current");
+  const currentDivSize = getElementSize(currentDiv);
+
+  // Difference between Current Div size and Button Size
+  const sizeDif = {
+    width: currentDivSize.width - buttonSize.width,
+    height: currentDivSize.height - buttonSize.height,
+  };
+
+  if (canClear) {
+    // Reset square
+    border.innerHTML = "";
+    border.id = "current";
+    border.style.backgroundColor = "white";
+    canClear = false;
+  }
+  if (sizeDif.width <= 0 || sizeDif.height <= 0) {
+    // Last Square
+    currentDiv.style.backgroundColor = getRandomColor();
+    canClear = true;
+  } else {
+    currentDiv.style.backgroundColor = getRandomColor();
+    currentDiv.id = "";
+
+    const newDiv = document.createElement("div");
+    newDiv.id = "current";
+    currentDiv.appendChild(newDiv);
+  }
 });
+
+// Helper functions
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+}
+
+function getElementSize(element) {
+  let { width, height } = element.getBoundingClientRect();
+  width = Math.floor(width);
+  height = Math.floor(height);
+  return { width, height };
+}
